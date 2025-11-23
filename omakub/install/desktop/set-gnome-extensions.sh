265ab9a509bcc8f379d5b09de7633af23e34ad00
Ben McIntyre <ben@bens-mac-mini.tailfc3929.ts.net>
2025-11-23 21:22:48 +0800

    Disable gum confirm in set-gnome-extensions.sh
    This was blocking the installation when running without a TTY

--- omakub/install/desktop/set-gnome-extensions.sh
+++ omakub/install/desktop/set-gnome-extensions.sh
@@ -10,7 +10,8 @@ gnome-extensions disable ubuntu-dock@ubuntu.com
 gnome-extensions disable ding@rastersoft.com
 
 # Pause to assure user is ready to accept confirmations
-gum confirm "To install Gnome extensions, you need to accept some confirmations. Ready?"
+# DISABLED: gum confirm hangs without TTY
+# gum confirm "To install Gnome extensions, you need to accept some confirmations. Ready?"
 
 # Install new extensions
 gext install tactile@lundal.io
