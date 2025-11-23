551180f54d08ebf91ddf9d6acd2efe7e1579570b
Ben McIntyre <ben@bens-mac-mini.tailfc3929.ts.net>
2025-11-23 21:55:29 +0800

    Fix OMAKUB_PATH not set during installation
    Set OMAKUB_PATH if not already defined so optional apps can be loaded

--- omakub/install/desktop/select-optional-apps.sh
+++ omakub/install/desktop/select-optional-apps.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Set OMAKUB_PATH if not already set
+if [[ -z "$OMAKUB_PATH" ]]; then
+	export OMAKUB_PATH="$HOME/.local/share/omakub"
+fi
+
 if [[ -v OMAKUB_FIRST_RUN_OPTIONAL_APPS ]]; then
 	apps=$OMAKUB_FIRST_RUN_OPTIONAL_APPS
 
